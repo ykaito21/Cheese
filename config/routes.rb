920aca3e85fcefa01a5281876020291776025317
Law WN <lawwnc@gmail.com>
2018-05-29 17:07:36 +0900

    fixed problem with order(s) in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :dishes, only: [ :index, :new, :create ]
     end
     resources :dishes, only: [ :show, :edit, :update, :destroy ] do
-      resources :order
+      resources :orders
     end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
