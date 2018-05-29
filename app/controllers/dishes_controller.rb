9c6ddedd43dd9ad3f6e94a26984f518113a37b38
Yuki Kaito <ykaito21@gmail.com>
2018-05-29 12:36:11 +0900

    add dishes controller

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,22 @@
+class DishesController < ApplicationController
+  def index
+    @dishes = Dishe.all
+  end
+
+  def show
+  end
+
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name, :description, :picture, :price, :category)
+  end
+
+  def set_dish
+    @dish = Dish.find(params[:id])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+end
