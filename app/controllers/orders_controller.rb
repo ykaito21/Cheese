a7f8c1c08ec101d56398ec71c06d7f7381c8da30
Josias Geoffroy <geoffroyjosias@mac.com>
2018-05-29 14:11:30 +0900

    added the show and create method in orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,25 @@ class OrdersController < ApplicationController
   end
 
   def show
+    @dish = Dish.find(params[:id])
+    @orders.dish = @dish
+  end
+
+
+  def create
+
+    @order = Order.new(order_params)
+    @orders.dish = @dish
+
+    @order.save
+    redirect_to order_path(@order)
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:date, :confirmation, :user_id, :dish_id)
 
   end
 end
