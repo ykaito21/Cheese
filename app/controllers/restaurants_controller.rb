dd9bfec9d120e1247e2c4a5adbdfb2f865c18e67
Yuki Kaito <ykaito21@gmail.com>
2018-05-29 15:42:31 +0900

    created restaurants controller and delete routes for restaurant index

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,46 @@
+class RestaurantsController < ApplicationController
+ before_action :set_restaurant, only: [ :show, :edit, :update, :destroy ]
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      redener :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurnt.find(params[:id])
+  end
+end
