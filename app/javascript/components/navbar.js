17283e7d6bdf375eb27698ca854044486d46dcbc
Tomomi Shiba <follwthedot@gmail.com>
2018-06-01 15:04:43 +0900

    toggle search nav depends on page

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,16 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   const banner = document.querySelector('.banner');
+  const search = document.querySelector('.search-container ');
   const bannerHeight = banner.clientHeight;
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= bannerHeight) {
         navbar.classList.add('navbar-fixed-top');
+        search.classList.add('show-search-container');
       } else {
         navbar.classList.remove('navbar-fixed-top');
+        search.classList.remove('show-search-container');
       }
     });
   }
